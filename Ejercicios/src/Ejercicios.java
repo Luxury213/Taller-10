a25cf7c82dd230da755ed4a71cf1ee78cce2d397
Estudiante USC <estudiante@GTACAD.USC>
2022-09-30 15:54:16 -0500

    Se agrega el proyecto

---
+++ Ejercicios/src/Ejercicios.java
@@ -0,0 +1,16 @@
+public class Ejercicios {
+
+	public static void main(String[] args)
+	{
+		byte b=45;
+		short s=0;
+		long ln=89;
+		int i=2, j=4;
+		float f=3;
+		double d=2.0;
+		char c='a';
+
+		 byte result=j – 4L;
+	System.out.println(result);
+	}
+}
---
+++ Ejercicios/src/ejerciciosapo/ContarEnteros.java
@@ -0,0 +1,29 @@
+package ejerciciosapo;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ContarEnteros {
+	
+	public static void main(String[] args) throws IOException {
+		
+	try (Scanner sc = new Scanner(System.in)) {
+		int n, cifras;
+        char car;
+        do{
+            System.out.print("Introduce un número entero: ");
+            n = sc.nextInt();
+            cifras= 0;   
+            while(n!=0){             
+                    n = n/10;         
+                   cifras++;
+            }
+            System.out.println("El número tiene " + cifras+ " cifras");
+            System.out.print("Continuar? ");
+            car = (char)System.in.read();
+        }while(car!='n' && car != 'N');
+	}   
+    }
+
+}
+
